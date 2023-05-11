90d0e96cb342d1c3d8143ad0d9fc3024a7c3932d
caifengze <fengzcw@gmail.com>
2023-05-11 17:27:14 +0800

    fix: refactor name of routes

--- routes.go
+++ routes.go
@@ -8,9 +8,10 @@ import (
 func CollectRoutes(r *gin.Engine) *gin.Engine {
 	// GET
 	r.GET("/Hello", controller.Hello)
-	r.GET("/UserBaseInfo", controller.UserbaseInfor)
+	r.GET("/UserBaseInfo", controller.UserBaseInfor)
 	// POST
 	r.POST("/UserLogin", controller.UserLogin)
-
+	// r.POST("/UserRegister", controller.UserRegister)
+	r.POST("/UploadVideo", controller.VideoUpload)
 	return r
 }
