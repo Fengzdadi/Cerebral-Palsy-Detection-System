c48e81fc095275f458054a6ef4f055721b5ad219
caifengze <fengzcw@gmail.com>
2023-05-12 10:25:07 +0800

    fix: bug address mongo.ErrorNoDocuments

--- Database/DataMethods.go
+++ Database/DataMethods.go
@@ -3,74 +3,70 @@ package Database
 import (
 	"Cerebral-Palsy-Detection-System/model"
 	"context"
-	"encoding/json"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 )
 
-func UserCheck(UserID string, UserPassword string) bool {
+func UserCheck(UserID string, UserPassword string) int {
 	// 取出连接池中的连接
 	conn, err := Pool.Get()
 	defer Pool.Release(conn)
 	if conn == nil {
 		log.Fatal(err)
-		return false
+		return 0
 	}
 	if err != nil {
 		log.Fatal(err)
-		return false
+		return 0
 	}
 	// 取出用户密码
 	// 从数据库中取出用户密码
 	var result model.User
-	collection := conn.Database("CPDS").Collection("User")
+	collection := conn.Database("CPDS_TEST").Collection("User")
 	if collection == nil {
 		log.Fatal(err)
-		return false
+		return 0
 	}
 	err = collection.FindOne(context.Background(), bson.M{"Userid": UserID}).Decode(&result)
 	if err != nil {
-		log.Fatal(err)
-		return false
+		if err == mongo.ErrNoDocuments {
+			return 0
+		}
+		return 0
 	}
 	if result.Password == UserPassword {
-		return true
+		return 1
 	} else {
-		return false
+		return 0
 	}
 }
 
-func GetUserinfo(name string) []byte {
+func GetUserinfo(name string, user *model.User) {
 	conn, err := Pool.Get()
 	defer Pool.Release(conn)
 	if conn == nil {
 		log.Fatal(err)
-		return nil
 	}
 	if err != nil {
 		log.Fatal(err)
-		return nil
 	}
 
-	collection := conn.Database("CPDS").Collection("User")
+	collection := conn.Database("CPDS_TEST").Collection("User")
 	if collection == nil {
 		log.Fatal(err)
-		return nil
 	}
 
-	filter := bson.D{{"name", name}}
-
-	var user model.User
+	filter := bson.D{{"Username", name}}
 
 	err = collection.FindOne(context.TODO(), filter).Decode(&user)
 	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return
+		}
 		log.Fatal(err)
-		return nil
 	}
-	userJson, err := json.Marshal(user)
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	return userJson
+	fmt.Print(user)
+	return
 }
