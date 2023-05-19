48281d748faeb359289561919d96e3f39e4c4f92
caifengze <fengzcw@gmail.com>
2023-05-19 10:57:07 +0800

    fix: route update

--- routes.go
+++ routes.go
@@ -8,8 +8,9 @@ import (
 func CollectRoutes(r *gin.Engine) *gin.Engine {
 	// GET
 	r.GET("/Hello", controller.Hello)
-	r.GET("/UserBaseInfo", controller.UserBaseInfo)
+	r.GET("/VideoRes", controller.VideoRes)
 	// POST
+	r.POST("/UserBaseInfo", controller.UserBaseInfo)
 	r.POST("/UserLogin", controller.UserLogin)
 	// r.POST("/UserRegister", controller.UserRegister)
 	r.POST("/UploadVideo", controller.VideoUpload)
