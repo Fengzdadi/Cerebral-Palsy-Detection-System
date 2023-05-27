a1351af352cfe272c077d8119cebcc9c3caea192
caifengze <fengzcw@gmail.com>
2023-05-27 17:04:59 +0800

    fix: fix bug of ReturnVideoRes()

--- controller/GETRequest.go
+++ controller/GETRequest.go
@@ -2,10 +2,30 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 )
 
 func Hello(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Hello, world!",
 	})
+}
+
+func ReturnVideoRes(c *gin.Context) {
+	videoFile, err := os.Open(".\\Vprocessing\\output.mp4")
+	if err != nil {
+		c.JSON(500, gin.H{"message": "{err}"})
+		return
+	}
+	defer videoFile.Close()
+
+	c.Header("Content-Type", "video/mp4")
+
+	_, err = io.Copy(c.Writer, videoFile)
+	if err != nil {
+		c.JSON(500, gin.H{"message": "{err}"})
+		return
+	}
+
 }
