3ece7ea8ac0a64fb7939f85c15e546dc60b6ebf4
caifengze <fengzcw@gmail.com>
2023-07-09 19:47:28 +0800

    feat: now can get history result

--- routes.go
+++ routes.go
@@ -8,12 +8,24 @@ import (
 func CollectRoutes(r *gin.Engine) *gin.Engine {
 	// GET
 	r.GET("/Hello", controller.Hello)
+	// Return video which can show in the front-end
 	r.GET("/VideoRes", controller.ReturnVideoRes)
+
+	// Database part
+	// For someone can get the history of result from database
+	// The return is array
+	r.GET("/UserHisResult", controller.GetHisResult)
+
 	// POST
+	// User part
 	r.POST("/UserBaseInfo", controller.UserBaseInfo)
 	r.POST("/UserLogin", controller.UserLogin)
 	// r.POST("/UserRegister", controller.UserRegister)
+
+	// Video part
 	r.POST("/UploadVideo", controller.VideoUpload)
 	r.POST("/StartDetection", controller.StartDetection)
+
+	// Database part
 	return r
 }
