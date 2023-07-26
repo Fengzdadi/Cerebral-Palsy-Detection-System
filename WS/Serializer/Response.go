d3dbd6b515ba25c02b6644e9e633d7170f7eff54
caifengze <fengzcw@gmail.com>
2023-07-27 00:08:59 +0800

    feat: response msg

---
+++ WS/Serializer/Response.go
@@ -0,0 +1,24 @@
+package Serializer
+
+type Response struct {
+	Code  int         `json:"code"`
+	Data  interface{} `json:"data"`
+	Msg   string      `json:"msg"`
+	Error string      `json:"error"`
+}
+
+type Datalist struct {
+	Item  interface{} `json:"item"`
+	Total int         `json:"total"`
+}
+
+type TokenData struct {
+	User  interface{} `json:"user"`
+	Token string      `json:"token"`
+}
+
+// TrackedErrorResponse 有追踪信息的错误反应
+type TrackedErrorResponse struct {
+	Response
+	TrackID string `json:"track_id"`
+}
