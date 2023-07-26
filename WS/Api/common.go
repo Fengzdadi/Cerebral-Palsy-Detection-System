a273652f22b5178914e9ae1ff65b675a3d26239e
caifengze <fengzcw@gmail.com>
2023-07-27 00:04:12 +0800

    feat: ErrorResponse

---
+++ WS/Api/common.go
@@ -0,0 +1,38 @@
+package Api
+
+import (
+	"Cerebral-Palsy-Detection-System/WS/Conf"
+	_ "Cerebral-Palsy-Detection-System/WS/Conf"
+	"Cerebral-Palsy-Detection-System/WS/Serializer"
+	"encoding/json"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+)
+
+// For the ErrorResponse in the Api\user.go, it is a function that returns a Serializer.Response struct.
+func ErrorResponse(err error) Serializer.Response {
+	if ve, ok := err.(validator.ValidationErrors); ok {
+		for _, e := range ve {
+			field := Conf.T(fmt.Sprintf("Field.%s", e.Field))
+			tag := Conf.T(fmt.Sprintf("Tag.Valid.%s", e.Tag))
+			return Serializer.Response{
+				Code:  400,
+				Msg:   fmt.Sprintf("%s%s", field, tag),
+				Error: fmt.Sprint(err),
+			}
+		}
+	}
+	if _, ok := err.(*json.UnmarshalTypeError); ok {
+		return Serializer.Response{
+			Code:  400,
+			Msg:   "JSON类型不匹配",
+			Error: fmt.Sprint(err),
+		}
+	}
+
+	return Serializer.Response{
+		Code:  400,
+		Msg:   "参数错误",
+		Error: fmt.Sprint(err),
+	}
+}
