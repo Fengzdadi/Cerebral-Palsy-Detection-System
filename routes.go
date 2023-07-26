d87412f2860f71a22740fde9cebe581fcf679a5a
caifengze <fengzcw@gmail.com>
2023-07-27 00:09:11 +0800

    feat: add routes

--- routes.go
+++ routes.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"Cerebral-Palsy-Detection-System/WS/Api"
+	"Cerebral-Palsy-Detection-System/WS/service"
 	"Cerebral-Palsy-Detection-System/controller"
 	"github.com/gin-gonic/gin"
 )
@@ -16,16 +18,28 @@ func CollectRoutes(r *gin.Engine) *gin.Engine {
 	// The return is array
 	r.GET("/UserHisResult", controller.GetHisResult)
 
+	//Ws part
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}) // For test
+	r.GET("/ws", service.WsHandler)
+
 	// POST
 	// User part
 	r.POST("/UserBaseInfo", controller.UserBaseInfo)
 	r.POST("/UserLogin", controller.UserLogin)
-	// r.POST("/UserRegister", controller.UserRegister)
+	r.POST("/UserRegister", controller.UserRegister)
 
 	// Video part
 	r.POST("/UploadVideo", controller.VideoUpload)
 	r.POST("/StartDetection", controller.StartDetection)
 
 	// Database part
+
+	// ws
+	r.POST("user/register", Api.UserRegister)
+
 	return r
 }
