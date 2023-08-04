d153fbfc95944c5ec64e770e728ba6f681ead4ac
caifengze <fengzcw@gmail.com>
2023-08-04 16:39:02 +0800

    feat: add BaseInfoHis

---
+++ BaseInfoHis/BaseInfoHis.go
@@ -0,0 +1,26 @@
+package BaseInfoHis
+
+import (
+	"Cerebral-Palsy-Detection-System/WS/model"
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type BaseInfoHis struct {
+	gorm            gorm.Model
+	BelongToChildID int       `gorm:"BelongtoChildID"`
+	Time            time.Time `gorm:"Time"`
+	height          float64   `gorm:"height"`
+	weight          float64   `gorm:"weight"`
+}
+
+func (b *BaseInfoHis) GetBaseInfoHis(belongToChildID int) ([]BaseInfoHis, error) {
+	var bInfoHis []BaseInfoHis
+	err := model.DB.Table("BaseInfoHis").Where("BelongToChildID = ?", belongToChildID).Find(&bInfoHis).Error
+	return bInfoHis, err
+}
+
+func (b *BaseInfoHis) AddBaseInfoHis() error {
+	err := model.DB.Table("BaseInfoHis").Create(&b).Error
+	return err
+}
