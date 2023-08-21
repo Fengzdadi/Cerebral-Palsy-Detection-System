1d8d6d0f4c449d25e912f74c269aa56c21c8c6b3
caifengze <fengzcw@gmail.com>
2023-08-21 19:21:42 +0800

    feat: logical refinement

--- model/BaseInfoHis.go
+++ model/BaseInfoHis.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"Cerebral-Palsy-Detection-System/WS/Pkg/e"
+	"Cerebral-Palsy-Detection-System/WS/Serializer"
 	"github.com/jinzhu/gorm"
+	logging "github.com/sirupsen/logrus"
 	"time"
 )
 
@@ -13,13 +16,39 @@ type BaseInfoHis struct {
 	weight          float64   `gorm:"weight"`
 }
 
-func GetBaseInfoHis(belongToChildID int) ([]BaseInfoHis, error) {
+func GetBaseInfoHis(belongToChildID int) ([]BaseInfoHis, Serializer.Response) {
 	var bInfoHis []BaseInfoHis
+	code := e.SUCCESS
 	err := DB.Table("BaseInfoHis").Where("BelongToChildID = ?", belongToChildID).Find(&bInfoHis).Error
-	return bInfoHis, err
+	if err != nil {
+		logging.Info(err)
+		code := e.ERROR
+		return bInfoHis, Serializer.Response{
+			Code: code,
+			Msg:  e.GetMsg(code),
+		}
+	} else {
+		return bInfoHis, Serializer.Response{
+			Code: code,
+			Msg:  e.GetMsg(code),
+		}
+	}
 }
 
-func (b *BaseInfoHis) AddBaseInfoHis() error {
+func (b *BaseInfoHis) AddBaseInfoHis() Serializer.Response {
+	code := e.SUCCESS
 	err := DB.Table("BaseInfoHis").Create(&b).Error
-	return err
+	if err != nil {
+		code = e.ERROR
+		return Serializer.Response{
+			Code: code,
+			Msg:  e.GetMsg(code),
+			Data: "",
+		}
+	} else {
+		return Serializer.Response{
+			Code: code,
+			Msg:  e.GetMsg(code),
+		}
+	}
 }
