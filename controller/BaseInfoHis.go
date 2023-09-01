d1f92bbccf51698858ff003625a9d129e37cc9c0
caifengze <fengzcw@gmail.com>
2023-09-01 22:32:02 +0800

    feat: fix the logic and simplify code

--- controller/BaseInfoHis.go
+++ controller/BaseInfoHis.go
@@ -2,31 +2,60 @@ package controller
 
 import (
 	"Cerebral-Palsy-Detection-System/model"
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func GetBaseInfoHis(c *gin.Context) {
-	session := sessions.Default(c)
-	session.Get("mySession")
-	userid := session.Get("mySession")
-	b, err := model.GetBaseInfoHis(userid.(int))
+	// The problem here is you cannot ensure the childId will be found by others when they request by this GET method
+	childId := c.Query("ChildId")
+	childIdInt, _ := strconv.Atoi(childId)
+	childIdUint := uint(childIdInt)
+	b, err := model.GetBaseInfoHis(childIdUint)
 	c.JSON(200, gin.H{
 		"message": b,
 		"error":   err,
 	})
+
+	//userNameValue, exists := c.Get("username")
+	//if !exists {
+	//	c.JSON(http.StatusBadRequest, gin.H{"error": "user_name not found"})
+	//	return
+	//} else {
+	//	fmt.Println(userNameValue)
+	//}
+	//
+	//userid, exists := c.Get("userid")
+	//
+	//// 写一个断言函数
+	//if !exists {
+	//	c.JSON(http.StatusBadRequest, gin.H{"error": "userid not found"})
+	//	return
+	//} else {
+	//	fmt.Println(userid)
+	//	fmt.Printf("Type of userid: %T\n", userid)
+	//}
+	//
+	//if floatUserID, ok := userid.(float64); ok {
+	//	uintUserID := uint(floatUserID)
+	//	b, err := model.GetBaseInfoHis(uintUserID)
+	//	c.JSON(200, gin.H{
+	//		"message": b,
+	//		"error":   err,
+	//	})
+	//} else {
+	//	c.JSON(400, gin.H{"error": "userid not found"})
+	//	return
+	//}
+
 }
 
 func AddBaseInfoHis(c *gin.Context) {
 	var base model.BaseInfoHis
-	if err := c.ShouldBindJSON(&base); err != nil {
+	if err := c.ShouldBind(&base); err != nil {
 		c.JSON(400, gin.H{"message": err.Error()})
 		return
 	}
-	session := sessions.Default(c)
-	session.Get("mySession")
-	userid := session.Get("mySession")
-	base.BelongToChildID = userid.(int)
 	err := base.AddBaseInfoHis()
 	c.JSON(200, err)
 }
