f610b49b5f7362e65a795e57e343cbe19ea98afa
caifengze <fengzcw@gmail.com>
2023-09-01 22:33:03 +0800

    feat: reduce the likelihood of errors and simplify code

--- controller/ChildrenInfo.go
+++ controller/ChildrenInfo.go
@@ -3,31 +3,34 @@ package controller
 import (
 	"Cerebral-Palsy-Detection-System/Apps/WsApi"
 	"Cerebral-Palsy-Detection-System/model"
-	"fmt"
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	logging "github.com/sirupsen/logrus"
 )
 
 func AddChildInfo(c *gin.Context) {
 	var child model.ChildrenInfo
-	if err := c.ShouldBind(&child); err == nil {
-		res := child.AddChildInfo()
-		c.JSON(200, res)
-	} else {
-		c.JSON(400, WsApi.ErrorResponse(err))
-		logging.Info(err)
+	userid, _ := c.Get("userid")
+	if floatUserID, ok := userid.(float64); ok {
+		uintUserID := uint(floatUserID)
+		if err := c.ShouldBind(&child); err == nil {
+			res := child.AddChildInfo(uintUserID)
+			c.JSON(200, res)
+		} else {
+			c.JSON(400, WsApi.ErrorResponse(err))
+			logging.Info(err)
+		}
 	}
+
 }
 
 func GetChildInfo(c *gin.Context) {
-	session := sessions.Default(c)
-	userid := session.Get("mySession")
-	fmt.Print(userid)
-	id := userid.(uint)
-	ch, err := model.GetChildInfo(id)
-	c.JSON(200, gin.H{
-		"message": ch,
-		"error":   err,
-	})
+	userid, _ := c.Get("userid")
+	if floatUserID, ok := userid.(float64); ok {
+		uintUserID := uint(floatUserID)
+		ch, res := model.GetChildInfo(uintUserID)
+		c.JSON(200, gin.H{
+			"message": ch,
+			"res":     res,
+		})
+	}
 }
