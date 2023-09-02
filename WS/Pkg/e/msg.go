4d7e29cd33f4fc71a6697db6440ca4be3a77d134
caifengze <fengzcw@gmail.com>
2023-09-02 11:13:58 +0800

    doc: update the name of error

--- WS/Pkg/e/msg.go
+++ WS/Pkg/e/msg.go
@@ -3,7 +3,7 @@ package e
 var MsgFlags = map[int]string{
 	SUCCESS:                 "ok",
 	UpdatePasswordSuccess:   "修改密码成功",
-	NotExistInentifier:      "该第三方账号未绑定",
+	NotExistIdentifier:      "该第三方账号未绑定",
 	ERROR:                   "fail",
 	InvalidParams:           "请求参数错误",
 	ErrorDatabase:           "数据库操作出错,请重试",
